64fb462989e8abaff135799baede3aa7372ed4ec
Christian LaVerde <chrislaverde1@gmail.com>
2025-03-25 22:20:24 -0500

    Update app to utilize passport, add authController/Routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,18 +2,39 @@ import type { Request, Response } from 'express';
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import userRoutes from './routes/user.routes.js';
+import session from 'express-session';
+import dotenv from 'dotenv';
+import passport from './config/passportConfig.js';
+import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+
 
 const app = express();
+dotenv.config();
 
-app.use(cors());
-app.use(cookieParser());
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+const sessionSecret = process.env.SESSION_SECRET;
+if (!sessionSecret) {
+  throw new Error('SESSION_SECRET environment variable must be set');
+}
+app.use(session({
+  secret: sessionSecret,
+  resave: false,
+  saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.get('/', (req: Request, res: Response) => {
   res.send("Hello, world!");
 });
 
+app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
 
 app.use((err: Error, req: Request, res: Response, next: Function) => {
