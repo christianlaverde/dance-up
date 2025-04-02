8237ccfd07ff98d47b683cac181c01bd62488a0d
Christian LaVerde <chrislaverde1@gmail.com>
2025-04-02 17:16:58 -0500

    Add id, ownerId, address, and corresponding getters back to studio.ts

--- backend/src/domain/studio.ts
+++ backend/src/domain/studio.ts
@@ -1,22 +1,36 @@
 import { Class } from "./class.js";
 
 export class Studio {
+  private id: string;
+  private ownerId: string;
   private studioName: string;
+  private address: string;
   private classes: Class[] = [];
 
-  constructor(studioName: string) {
+  constructor(id: string, ownerId: string, studioName: string, address: string) {
+    this.id = id;
+    this.ownerId = ownerId;
     this.studioName = studioName;
+    this.address = address;
   }
 
   addClass(cls: Class) {
     this.classes.push(cls);
   }
 
-  getClasses() {
-    return this.classes;
+  getId() {
+    return this.id;
+  }
+
+  getOwnerId() {
+    return this.ownerId;
   }
 
   getStudioName() {
     return this.studioName;
+  }
+
+  getAddress() {
+    return this.address;
   }
 }
