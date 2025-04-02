46abcd4799afda6601287e4ecb571242d3698756
Christian LaVerde <chrislaverde1@gmail.com>
2025-04-02 17:16:22 -0500

    Update IStudioRepository to possibly return null for getStudioById

--- backend/src/repositories/IStudioRepository.ts
+++ backend/src/repositories/IStudioRepository.ts
@@ -2,6 +2,6 @@ import { Studio } from "../domain/studio.js"
 
 export interface IStudioRepository {
   getAllStudios(): Promise<Studio[]>;
-  getStudioById(id: string): Promise<Studio>;
+  getStudioById(id: string): Promise<Studio | null>;
   saveStudio(studio: Studio): Promise<void>;
 }
