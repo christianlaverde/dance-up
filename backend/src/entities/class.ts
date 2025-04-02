40cf1d2affb8acb58a5488f4de0d50105fccac9e
Christian LaVerde <chrislaverde1@gmail.com>
2025-04-02 16:27:58 -0500

    Simplify class entity to bare minimum

--- backend/src/entities/class.ts
+++ backend/src/entities/class.ts
@@ -1,47 +1,11 @@
 export class Class {
-  private id: string;
-  private studioId: string;
   private className: string;
   private classDescription: string;
 
   constructor(
     id: string, studioId: string, className: string, classDescription: string
   ) {
-    this.id = id;
-    this.studioId = studioId;
     this.className = className;
-    this.classDescription = classDescription;
-  }
-
-  getId(): string {
-    return this.id;
-  }
-
-  setId(id: string): void {
-    this.id = id;
-  }
-
-  getStudioId(): string {
-    return this.studioId;
-  }
-
-  setStudioId(studioId: string): void {
-    this.studioId = studioId;
-  }
-
-  getClassName(): string {
-    return this.className;
-  }
-
-  setClassName(className: string): void {
-    this.className = className;
-  }
-
-  getClassDescription(): string {
-    return this.classDescription;
-  }
-
-  setClassDescription(classDescription: string): void {
     this.classDescription = classDescription;
   }
 }
