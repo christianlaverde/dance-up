dbbf7f17f767344168eb1d2e1fff6fa8367665d5
Christian LaVerde <chrislaverde1@gmail.com>
2025-04-01 21:25:28 -0500

    Fix indentation in db/db.ts

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -12,24 +12,24 @@ const { Pool } = pg;
  * It provides a simple query method to execute queries against the database.
  */
 export class Database {
-    private pool: pg.Pool;
-  
-    constructor() {
-      // Initialize the connection pool
-      this.pool = new Pool();
-    }
-  
-    /**
-     * Executes a database query using the underlying connection pool.
-     *
-     * @param queryTextOrConfig - The query string or configuration object.
-     * @param values - Optional array of query parameter values.
-     * @returns A promise that resolves to the query result.
-     */
-    public async query<R extends QueryResultRow = any, I extends any[] = any[]>(
-      queryTextOrConfig: string | QueryConfig<I>,
-      values?: QueryConfigValues<I>
-    ): Promise<QueryResult<R>> {
-      return this.pool.query<R, I>(queryTextOrConfig, values);
-    }
+  private pool: pg.Pool;
+
+  constructor() {
+    // Initialize the connection pool
+    this.pool = new Pool();
   }
+
+  /**
+   * Executes a database query using the underlying connection pool.
+   *
+   * @param queryTextOrConfig - The query string or configuration object.
+   * @param values - Optional array of query parameter values.
+   * @returns A promise that resolves to the query result.
+   */
+  public async query<R extends QueryResultRow = any, I extends any[] = any[]>(
+    queryTextOrConfig: string | QueryConfig<I>,
+    values?: QueryConfigValues<I>
+  ): Promise<QueryResult<R>> {
+    return this.pool.query<R, I>(queryTextOrConfig, values);
+  }
+}
