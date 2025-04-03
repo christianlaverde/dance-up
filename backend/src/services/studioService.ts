cababd6acf176f48dd4187309c10bc1f526472fb
Christian LaVerde <chrislaverde1@gmail.com>
2025-04-03 00:35:34 -0500

    Update StudioService to use StudioRepository and ClassRepository and update getAllStudios

--- backend/src/services/studioService.ts
+++ backend/src/services/studioService.ts
@@ -1,33 +1,19 @@
 /**
  * StudioService
- *
- * This service encapsulates all business logic related to studios.
- * It acts as a bridge between the controller layer and the data access layer (StudioModel).
- * Each method here delegates data retrieval or manipulation tasks to the StudioModel,
- * while also handling any business-specific rules or error conditions.
- * 
  */
 
-import type { StudioModel } from "../models/studioModel.js";
 import type { Studio } from "../domain/studio.js";
-//import type { UserService } from "./userService.js";
-import type { User } from '../entities/user.js';
-
-
+import type { Class } from "../domain/class.js";
+import { IStudioRepository } from "../repositories/IStudioRepository.js";
+import { IClassRepository } from "../repositories/IClassRepository.js";
 
 export class StudioService {
-  // Instance of StudioModel used to interact with the database.
-  private studioModel: StudioModel;
-  //private userService: UserService;
+  private studioRepository: IStudioRepository;
+  private classRepository: IClassRepository;
 
-  /**
-   * Constructor for StudioService.
-   * @param studioModel - An instance of StudioModel for data access operations.
-   */
-  //constructor(studioModel: StudioModel, userService: UserService) {
-  constructor(studioModel: StudioModel) {
-    this.studioModel = studioModel;
-    //this.userService = userService;
+  constructor(studioRepository: IStudioRepository, classRepository: IClassRepository) {
+    this.studioRepository = studioRepository;
+    this.classRepository = classRepository;
   }
 
   /**
@@ -35,22 +21,30 @@ export class StudioService {
    * @returns Promise that resolves to an array of Studio entities.
    */
   async getAllStudios(): Promise<Studio[]> {
-    const studios = await this.studioModel.getAllStudios();
+    const studios = await this.studioRepository.getAllStudios();
+    await Promise.all(studios.map(async (studio) => {
+      const studioId = studio.getId();
+      const classes = await this.classRepository.getClassesByStudioId(studioId);
+      studio.setClasses(classes);
+    }));
     return studios;
   }
 
+/*
   async getStudioById(id: string): Promise<Studio | null> {
     const studio = await this.studioModel.getStudioById(id);
     return studio || null;
   }
-
+*/
   /**
   * Retrieve all studio members from a studio given a studio id.
   * @param studioId - the id of the studio
   * @returns Promise that resolves to an array of .
   */
+/*
   async getAllStudioMembers(studioId: string): Promise<User[]> {
     const studios = await this.studioModel.getAllStudioMembers(studioId);
     return studios;
   }
+*/
 }
