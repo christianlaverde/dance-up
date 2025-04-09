8cd0680faac4a919aaf9a8776d1842951169dad1
Christian LaVerde <chrislaverde1@gmail.com>
2025-04-08 20:03:30 -0500

    Update Class entity to include DAY_OF_WEEK enum and simplified get method names

--- backend/src/domain/class.ts
+++ backend/src/domain/class.ts
@@ -1,33 +1,42 @@
-import { DateTime } from 'luxon';
+export enum DAY_OF_WEEK {
+  // ISO8601 Standard
+  MONDAY = 1,
+  TUESDAY = 2,
+  WEDNESDAY = 3,
+  THURSDAY = 4,
+  FRIDAY = 5,
+  SATURDAY = 6,
+  SUNDAY = 7
+}
 
 export class Class {
   private id: string;
-  private studioId: string;
   private name: string;
   private description: string;
+  private day: DAY_OF_WEEK;
 
   constructor(
-    classId: string, studioId: string, className: string, classDescription: string
+    classId: string, className: string, classDescription: string, day: DAY_OF_WEEK
   ) {
     this.id = classId;
-    this.studioId = studioId;
     this.name = className;
     this.description = classDescription;
+    this.day = day;
   }
 
   getId() {
     return this.id;
   }
 
-  getStudioId() {
-    return this.studioId;
-  }
-
-  getClassName() {
+  getName() {
     return this.name;
   }
 
-  getClassDescription() {
+  getDescription() {
     return this.description;
+  }
+
+  getDay() {
+    return this.day;
   }
 }
