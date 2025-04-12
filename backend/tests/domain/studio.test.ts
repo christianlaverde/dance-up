d4189e7d6978d8b5b0c96ea642fdff620a2b0811
Christian LaVerde <chrislaverde1@gmail.com>
2025-04-11 23:59:32 -0500

    Update studio.test.ts to be in line with new Class updates

--- backend/tests/domain/studio.test.ts
+++ backend/tests/domain/studio.test.ts
@@ -5,7 +5,16 @@ import { Class, DAY_OF_WEEK} from '../../src/domain/class.js';
 describe('Studio Entity', () => {
   it('should add a class', async () => {
     // Arrange 
-    const cls = new Class('class-1', 'Salsa Class', 'A fun salsa class', DAY_OF_WEEK.MONDAY);
+    const cls = new Class({
+      id: 'class-1',
+      name: 'Salsa Class',
+      timeSlot: {
+        day: DAY_OF_WEEK.MONDAY,
+        startHour: 20,
+        startMinute: 30,
+        durationMinutes: 90
+      }
+    });
     const studio = new Studio('studio-1', '1', 'VB Dance Studio', '123 Main St.');
 
     // Act
@@ -18,8 +27,28 @@ describe('Studio Entity', () => {
 
   it('should return a collection of classes', async () => {
     // Arrange
-    const cls1 = new Class('class-1', 'Salsa Class', 'A fun salsa class');
-    const cls2 = new Class('class-2', 'Jazz Class', 'A fun jazz class', DAY_OF_WEEK.WEDNESDAY);
+    const cls1 = new Class({
+      id: 'class-1', 
+      name: 'Salsa Class', 
+      timeSlot: {
+        day: DAY_OF_WEEK.MONDAY,
+        startHour: 20,
+        startMinute: 30,
+        durationMinutes: 90
+      },
+      description: 'A fun salsa class'
+    });
+    const cls2 = new Class({
+      id: 'class-2', 
+      name: 'Jazz Class',
+      timeSlot: {
+        day: DAY_OF_WEEK.WEDNESDAY,
+        startHour: 20,
+        startMinute: 30,
+        durationMinutes: 90        
+      },
+      description: 'A fun jazz class'
+    });
     const mockClasses: Class[] = [cls1, cls2];
     const studio = new Studio('1', '1', 'VB Dance Studio', '123 Main St.');
 
@@ -28,7 +57,7 @@ describe('Studio Entity', () => {
 
     // Act
     const classes = studio.getClasses();
-
+    
     // Assert
     expect(classes).toEqual(mockClasses);
   });
